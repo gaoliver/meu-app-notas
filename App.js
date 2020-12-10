ff840ac059ce79e0a15280ead5e444fbdf4afd72
gaoliver <gabriel.o.c.ramos@gmail.com>
2020-12-10 15:25:14 -0300

    Novo Design + Modal (design)

--- App.js
+++ App.js
@@ -9,13 +9,19 @@ import {
   FlatList,
   View,
   Modal,
-  Alert,
+  Text,
 } from "react-native";
 
 import ListaNotas from "./Components/ListaNotas";
 import Header from "./Components/Header";
 
 export default function App() {
+  const [inputNota, setInputNota] = useState("");
+
+  const notaInput = (addTexto) => {
+    setInputNota(addTexto);
+  };
+
   const [Notas, setNotas] = useState([]);
 
   const adicionarNota = (tituloNota) => {
@@ -23,6 +29,7 @@ export default function App() {
       ...notasAtuais,
       { id: Math.random().toString(), value: tituloNota },
     ]);
+    setInputNota("");
   };
 
   const removeNota = (notaId) => {
@@ -38,7 +45,12 @@ export default function App() {
       <StatusBar barStyle="default" />
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <SafeAreaView style={styles.container}>
-          <Header adicionarNotaBtn={adicionarNota} />
+          <Header
+            adicionarNotaBtn={adicionarNota}
+            onChangeText={notaInput}
+            value={inputNota}
+            onPress={inputNota}
+          />
           <FlatList
             inverted
             style={styles.NotesField}
@@ -74,12 +86,32 @@ export default function App() {
             statusBarTranslucent={true}
           >
             <View style={styles.ModalCorpo}>
-              <TouchableWithoutFeedback
-                onPress={() => setModalVisible(false)}
-              >
+              <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
                 <View style={{ width: "100%", height: "100%" }}></View>
               </TouchableWithoutFeedback>
-              <View style={styles.NotaCorpo}></View>
+              <View style={styles.NotaCorpo}>
+                <View style={styles.NotaHeader}>
+                  <Text style={styles.NotaTitle}>Título da nota</Text>
+                  <Text style={styles.NotaDate}>dd/mm/aa hh:mm</Text>
+                </View>
+                <View style={styles.NotaCampo}>
+                  <Text style={styles.NotaCampoText}>
+                    Donec imperdiet justo arcu, sit amet laoreet orci fermentum
+                    vitae. Etiam maximus nisl mi, vitae accumsan orci malesuada
+                    eu. In in sem aliquam, laoreet ex eu, mollis elit. Ut vitae
+                    sapien et nunc volutpat accumsan. Vestibulum faucibus odio
+                    elit, vel rhoncus quam tincidunt at. Suspendisse consequat
+                    sapien a dolor tempor egestas.
+                    {"\n\n"}Nullam non euismod
+                    velit, sit amet bibendum metus. Nam vulputate sagittis
+                    condimentum. In hac habitasse platea dictumst. Suspendisse
+                    sed ligula sit amet ipsum tincidunt cursus. Fusce aliquet
+                    nisl non scelerisque ornare. Aliquam interdum hendrerit
+                    diam, imperdiet tincidunt felis porttitor eget. Nunc
+                    fermentum euismod neque, ut lacinia ante venenatis a.
+                  </Text>
+                </View>
+              </View>
             </View>
           </Modal>
         </SafeAreaView>
@@ -92,7 +124,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: Dimensions.get("screen").width,
-    backgroundColor: "#ddd",
+    backgroundColor: "#bbb",
     paddingHorizontal: 10,
     paddingTop: 30,
     alignItems: "center",
@@ -119,5 +151,33 @@ const styles = StyleSheet.create({
     height: 500,
     backgroundColor: "#fff",
     position: "absolute",
+    borderRadius: 10,
+    padding: 10,
   },
+  NotaHeader: {
+    flexDirection: "row",
+    width: "100%",
+    height: 50,
+    paddingTop: 10,
+    marginTop: 0,
+    // backgroundColor: "#f5f5f5",
+  },
+  NotaTitle: {
+    width: "80%",
+    fontSize: 20,
+  },
+  NotaDate: {
+    width: "20%",
+    textAlign: "right",
+    fontSize: 12,
+    color: "#888",
+  },
+  NotaCampo: {
+    width: "100%",
+    height: 420,
+    marginTop: 5,
+  },
+  NotaCampoText: {
+    color: "#555",
+  }
 });
--- Components/Header.js
+++ Components/Header.js
@@ -9,26 +9,22 @@ import {
 } from "react-native";
 
 const Header = (props) => {
-  const [inputNota, setInputNota] = useState("");
-
-  const notaInput = (addTexto) => {
-    setInputNota(addTexto);
-  };
-
   return (
     <View style={styles.Header}>
-      <TextInput
-        style={styles.Input}
-        placeholder="Adicionar anotação"
-        onChangeText={notaInput}
-        value={inputNota}
-      />
-      <TouchableOpacity
-        style={styles.Adicionar}
-        onPress={props.adicionarNotaBtn.bind(this, inputNota)}
-      >
-        <Text style={styles.TextBtn}>Adicionar</Text>
-      </TouchableOpacity>
+      <View style={styles.HeaderInner}>
+        <TextInput
+          style={styles.Input}
+          placeholder="Adicionar anotação"
+          onChangeText={props.onChangeText}
+          value={props.value}
+        />
+        <TouchableOpacity
+          style={styles.Adicionar}
+          onPress={props.adicionarNotaBtn.bind(this, props.onPress)}
+        >
+          <Text style={styles.TextBtn}>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -38,26 +34,37 @@ export default Header;
 const styles = StyleSheet.create({
   Header: {
     width: Dimensions.get("screen").width,
-    flexDirection: "row",
     height: 80,
     paddingTop: 10,
   },
   Input: {
-    width: "70%",
+    width: "72%",
     fontSize: 20,
     justifyContent: "center",
     marginLeft: "3%",
-    marginRight: "2%",
+    marginRight: "2.1%",
   },
   Adicionar: {
     width: "23%",
-    backgroundColor: "#d50001",
+    height: '100%',
+    backgroundColor: "#08ff",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 10,
   },
   TextBtn: {
     color: "#fff",
     fontSize: 18,
+  },
+  HeaderInner: {
+    flexDirection: "row",
+    width: "94%",
+    height: 67,
+    paddingLeft: "1%",
+    alignItems: "center",
+    alignSelf: "center",
+    marginVertical: 3,
+    backgroundColor: "#fff7",
+    borderRadius: 20,
+    overflow: 'hidden'
   },
 });
