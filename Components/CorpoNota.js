be09b9c691a5fda890f3ecec1f5b819582d7f049
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-11-01 11:32:13 -0300

    feat: Novo modal

--- Components/CorpoNota.js
+++ Components/CorpoNota.js
@@ -3,58 +3,44 @@ import {
   StyleSheet,
   Text,
   View,
-  Modal,
   TouchableWithoutFeedback,
   TextInput,
   Button,
 } from "react-native";
+import Modal from "react-native-modal"
 
 const CorpoNota = (props) => {
   return (
     <Modal
-      visible={props.visible}
-      transparent={true}
-      animationType="fade"
-      statusBarTranslucent={true}
+      isVisible={props.visible}
     >
-      <View style={styles.ModalFundo}></View>
-
-      <Modal
-        visible={props.visible}
-        transparent={true}
-        animationType="slide"
-        statusBarTranslucent={true}
-      >
-        <View style={styles.ModalCorpo}>
-          <TouchableWithoutFeedback onPress={props.close}>
-            <View style={{ width: "100%", height: "100%" }}></View>
-          </TouchableWithoutFeedback>
-          <View style={styles.NotaCorpo}>
-            <View style={styles.NotaHeader}>
-              <TextInput
-                style={styles.NotaTitle}
-                value={props.titulo}
-                onChangeText={(text) => props.onChangeTitle(text)}
-              />
-              <Text style={styles.NotaDate}>
-                {props.data + " " + props.hora}
-              </Text>
-            </View>
-            <View style={styles.NotaCampo}>
-              <TextInput
-                style={styles.NotaCampoText}
-                value={props.texto}
-                placeholder="Escreva aqui seu texto"
-                onChangeText={(text) => props.onChangeText(text)}
-                numberOfLines={50}
-                multiline
-                textAlignVertical="top"
-              />
-            </View>
-            <Button title="Salvar" onPress={props.onSave} />
+      <View style={styles.ModalCorpo}>
+        <TouchableWithoutFeedback onPress={props.close}>
+          <View style={{ width: "100%", height: "100%" }}></View>
+        </TouchableWithoutFeedback>
+        <View style={styles.NotaCorpo}>
+          <View style={styles.NotaHeader}>
+            <TextInput
+              style={styles.NotaTitle}
+              value={props.titulo}
+              onChangeText={(text) => props.onChangeTitle(text)}
+            />
+            <Text style={styles.NotaDate}>{props.data + " " + props.hora}</Text>
           </View>
+          <View style={styles.NotaCampo}>
+            <TextInput
+              style={styles.NotaCampoText}
+              value={props.texto}
+              placeholder="Escreva aqui seu texto"
+              onChangeText={(text) => props.onChangeText(text)}
+              numberOfLines={50}
+              multiline
+              textAlignVertical="top"
+            />
+          </View>
+          <Button title="Salvar" onPress={props.onSave} />
         </View>
-      </Modal>
+      </View>
     </Modal>
   );
 };
